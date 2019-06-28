362b6206918ca685a51939ff6466dfff3af31d99
guad-quincris <guad.quincris@hotmail.com>
2019-06-27 23:09:34 -0500

    juego ahorcado 1.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+@Component( {
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+} )
+
+export class AppComponent {
+  palabra = 'AGUACATE';
+  palabraOculta = '';
+
+  intento  = 0;
+
+  gano = false;
+  perdio = false;
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+  constructor() {
+      this.palabraOculta = '_ '.repeat( this.palabra.length );
+
+  }
+
+comprobar(letra) {
+  this.existeLetra(letra);
+  const palabraOcultaArr = this.palabraOculta.split(' ');
+
+  for ( let i = 0; i < this.palabra.length; i++ ) {
+
+    if ( this.palabra[i] === letra ) {
+      palabraOcultaArr[i] = letra;
+    }
+
+  }
+  this.palabraOculta = palabraOcultaArr.join(' ');
+  this.verificaGame();
+  }
+  verificaGame(){
+    console.log( this.palabraOculta );
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+    if( palabraEvaluar === this.palabra){
+      this.gano = true;
+    }
+    if( this.intento >=9){
+      this.perdio = true;
+    }
+  }
+
+  existeLetra( letra ){
+
+    if(this.palabra.indexOf( letra) >= 0 ){
+      console.log('Letra existe ' + letra);
+    } else {
+      console.log('letra NO existe ' + letra );
+      this.intento ++;
+    }
+  }
+}
